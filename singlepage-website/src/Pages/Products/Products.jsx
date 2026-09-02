86bc1afcfe1e2751d990a95682c7f9ed0f457a9d
Shoeb-target <shaikh.shoeb9657@gmail.com>
2026-09-02 12:08:10 +0530

    product data

--- singlepage-website/src/Pages/Products/Products.jsx
+++ singlepage-website/src/Pages/Products/Products.jsx
@@ -1,8 +1,43 @@
-import React from "react";
-import  InfoSection  from "../../compoents/InfoSection/InfoSection";
+import React, { useEffect } from "react";
+import InfoSection from "../../compoents/InfoSection/InfoSection";
 import { homeObjOne1, homeObjOne2 } from "../Data";
 
+const PRODUCT_IDS = [
+  "101", "102", "103", "104", "105", "106", "107", "108", "109", "110",
+  "201", "202", "203", "204", "205", "206", "207", "208", "209", "210"
+];
+
 function Products() {
+  useEffect(() => {
+    const randomProductId = PRODUCT_IDS[Math.floor(Math.random() * PRODUCT_IDS.length)];
+
+    const trySendProductView = (retries = 10) => {
+      if (window.alloy) {
+        window.alloy("sendEvent", {
+          renderDecisions: true,
+          xdm: {
+            eventType: "commerce.productViews",
+            productListItems: [
+              {
+                SKU: randomProductId
+              }
+            ],
+            commerce: {
+              productViews: {
+                value: 1
+              }
+            }
+          }
+        });
+        console.log("Product view sent for SKU:", randomProductId);
+      } else if (retries > 0) {
+        setTimeout(() => trySendProductView(retries - 1), 200);
+      }
+    };
+
+    trySendProductView();
+  }, []);
+
   return (
     <>
       <InfoSection {...homeObjOne1} />
